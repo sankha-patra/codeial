c9b8efbf1d05a218738195b0170b8c8904e8d6ba
sankha-patra <sankha2312@gmail.com>
2021-11-18 22:17:05 +0530

    deleting posts and comments

--- routes/comments.js
+++ routes/comments.js
@@ -3,7 +3,9 @@ const express=require("express");
 const router=express.Router();
 // importing from posts controller
 const commentsController = require("../controllers/comments_controller")
+// creating comments
 router.post("/create",passport.checkAuthentication,commentsController.create)
-
+// deleting comments
+router.get("/destroy/:id",passport.checkAuthentication,commentsController.destroy);
 
 module.exports = router;
--- routes/posts.js
+++ routes/posts.js
@@ -5,6 +5,6 @@ const router=express.Router();
 // importing from posts controller
 const postsController = require("../controllers/posts_controller")
 router.post("/create",passport.checkAuthentication,postsController.create)
-
+router.get("/destroy/:id",passport.checkAuthentication,postsController.destroy)
 
 module.exports = router;
